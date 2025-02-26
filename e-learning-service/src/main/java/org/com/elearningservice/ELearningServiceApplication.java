bb2236e96b8a4484bba0f6907415056a6b06afe9
nguyenhieu31 <hieufaptv777@gmail.com>
2025-02-26 11:53:41 +0700

    Add services as normal directories

---
+++ e-learning-service/src/main/java/org/com/elearningservice/ELearningServiceApplication.java
@@ -0,0 +1,13 @@
+package org.com.elearningservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ELearningServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ELearningServiceApplication.class, args);
+    }
+
+}
---
+++ e-learning-service/src/test/java/org/com/elearningservice/ELearningServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.com.elearningservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ELearningServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
