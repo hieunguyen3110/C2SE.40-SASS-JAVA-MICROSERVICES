36e92456f8f1469b25690b456c9d3a4cffdaee35
nguyenhieu31 <hieufaptv777@gmail.com>
2025-04-22 00:16:55 +0700

    feature: add send notify user join group

--- websocket-server/src/main/java/org/com/websocketserver/handler/KafkaConsumerHandler.java
+++ websocket-server/src/main/java/org/com/websocketserver/handler/KafkaConsumerHandler.java
@@ -53,6 +53,18 @@ public class KafkaConsumerHandler {
         }
     }
 
+    @KafkaListener(topics = "join-group-ws-topic", groupId = "websocket-group")
+    public void sendNotificationJoinRequest(ConsumerRecord<String, String> records) {
+        try{
+            String followerId= records.key();
+            String json= records.value();
+            NotificationDto notificationDto= objectMapper.readValue(json,NotificationDto.class);
+            simpMessagingTemplate.convertAndSendToUser(followerId, "/queue/notifications-with-join-request", notificationDto);
+        }catch (JsonProcessingException e){
+            log.error("Error: "+ e.getMessage());
+        }
+    }
+
     @KafkaListener(topics = "study-group-ws-topic", groupId = "websocket-group")
     public void sendNotificationStudyGroupForClient(ConsumerRecord<String, String> records) {
         try {
