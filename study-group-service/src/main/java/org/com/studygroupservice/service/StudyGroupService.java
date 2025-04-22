15dbc84dbdff5f15ff2c1bcbe25b91dab53a1bea
diitunggph <kingtung3@gmail.com>
2025-04-22 18:13:26 +0700

    - fix service

--- study-group-service/src/main/java/org/com/studygroupservice/service/StudyGroupService.java
+++ study-group-service/src/main/java/org/com/studygroupservice/service/StudyGroupService.java
@@ -32,7 +32,7 @@ public interface StudyGroupService {
     void addMember(Long groupId, Long userId);
 
     @Transactional
-    void joinGroup(Long groupId, AccountDto accountDto) throws Exception;
+    void joinGroup(Long groupId, Long accountId) throws Exception;
 
     void removeMember(Long groupId, Long userId);
 
