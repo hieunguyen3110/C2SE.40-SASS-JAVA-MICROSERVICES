3188e95777ae1ee088018e129dc0a3965abea4c3
nguyenhieu31 <hieufaptv777@gmail.com>
2025-05-26 21:13:18 +0700

    fix: fix api send message with chat ai

--- document-service/src/main/java/com/capstone1/sasscapstone1/repository/httpClient/ChatbotClient.java
+++ document-service/src/main/java/com/capstone1/sasscapstone1/repository/httpClient/ChatbotClient.java
@@ -1,8 +1,10 @@
 package com.capstone1.sasscapstone1.repository.httpClient;
 
+import com.capstone1.sasscapstone1.dto.ChatbotDTO.ChatbotResponse;
 import com.capstone1.sasscapstone1.dto.response.ApiResponse;
 import com.capstone1.sasscapstone1.enums.ErrorCode;
 import com.capstone1.sasscapstone1.exception.ApiException;
+import com.capstone1.sasscapstone1.request.SendMessageRequest;
 import com.capstone1.sasscapstone1.request.TrainDocumentRequest;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
@@ -14,6 +16,9 @@ public interface ChatbotClient {
     @CircuitBreaker(name="chatbot-service", fallbackMethod = "fallbackChatbot")
     @Retry(name = "chatbot-service")
     ApiResponse<String> trainFile(@RequestBody TrainDocumentRequest request);
+
+    @PostExchange("/search")
+    ApiResponse<ChatbotResponse> sendMessage(@RequestBody SendMessageRequest request);
 
     default ApiResponse<String> fallbackChatbot(TrainDocumentRequest request,Throwable throwable) {
         throw new ApiException(ErrorCode.BAD_REQUEST.getStatusCode().value(), "Oops! Something went wrong, please try chatbot again later!");
